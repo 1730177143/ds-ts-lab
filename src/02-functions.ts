c43c1695f9780b5d3dcb5d9a2096798c127ed4d3
Long Liu <1730177143@qq.com>
2024-01-28 22:02:48 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -62,6 +62,15 @@ function findFriends(friends: Friend[], condition: (f: Friend) => boolean): stri
     return friends.filter(condition).map((f) => f.name);
 }
 
-
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(f: Friend, interest: string): string[] {
+    if (!f.interests) {
+        f.interests = [];
+    }
+    f.interests.push(interest);
+    return f.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'))
